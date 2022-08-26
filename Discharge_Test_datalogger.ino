d84caf2b308ebb50f195743ac917951d7b8ac55c
FurTrader <32523605+FurTrader@users.noreply.github.com>
2022-08-26 16:37:04 -0400

    Update Discharge_Test_datalogger.ino

--- Discharge_Test_datalogger.ino
+++ Discharge_Test_datalogger.ino
@@ -4,6 +4,8 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
+#include <EEPROM.h>
+
 #define SDcardChipSelect 10
 
 #include "RTClib.h"
@@ -11,7 +13,8 @@ RTC_DS1307 rtc;
 //to set the RTC time, run the ds1307 example code from adafruit
 DateTime now;
 
-#include <EEPROM.h> //for the serial number assignment
+//global variables
+long lastmillis;
 int eeAddress = 0;   //Location we want the SN data to be put.
 int serialnumber = 1000; //starting serial number
 String filename = "test";
@@ -23,9 +26,6 @@ OverkillSolarBms2 bms;
 LiquidCrystal_I2C lcd(0x27,20,4);
 //LiquidCrystal_I2C lcd(0x3F,20,4); //Library object for the LCD
 //set the LCD address to 0x3F or 0x27 for a 20 chars and 4 line display
-
-//global variables
-long lastmillis;
 
 void setup() {
   //only execute this once to initialize the eeprom location. This will set the serial number to the initial value.
@@ -46,37 +46,39 @@ void setup() {
   lcd.noBlink();
   lcd.noCursor();
   lcd.clear();
-  lcd.print("Overkill Solar");
+  lcd.print(F("Overkill Solar"));
   lcd.setCursor(0, 2);
-  lcd.print("Data Recorder V1"); 
+  lcd.print(F("Data Recorder V1")); 
 
   delay(3000);
 
   //set up the SD card
   lcd.clear();
-  lcd.print("Initializing SD card");
+  lcd.print(F("Initializing SD card"));
   lcd.setCursor(0, 1);
   if (!SD.begin(SDcardChipSelect)) {
-    lcd.print("initialization fail");
+    lcd.print(F("initialization fail"));
     while (1);
   }
-  lcd.print("initialization OK.");
+  lcd.print(F("initialization OK."));
   lcd.setCursor(0, 2);
+  
   NewFile();//create a new file for this test
+  
   // Check to see if the file exists:
   if (SD.exists(filename)) {
    lcd.println("file: " + filename);
   } else {
-    lcd.println(filename + "doesn't exist.");
+    lcd.println(filename + " NFG");
   }
   lcd.setCursor(0, 3);
 
   //set up the RTC
   if (! rtc.begin()) {
-    lcd.println("Couldn't find RTC");
+    lcd.println(F("Couldn't find RTC"));
   }
   if (! rtc.isrunning()) {
-    lcd.println("RTC NOT running!");
+    lcd.println(F("RTC NOT running!"));
     // When time needs to be set on a new device, or after a power loss, the
     // following line sets the RTC to the date & time this sketch was compiled
     //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
@@ -85,7 +87,7 @@ void setup() {
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }else{
     now = rtc.now();
-    lcd.print("unix time:");
+    lcd.print(F("unix time:"));
     lcd.print(now.unixtime());
   }
   
@@ -121,20 +123,92 @@ void loop() {
   //1 second timer, non-blocking
   if ((millis() - lastmillis) > 1000){ 
     lastmillis = millis();
-    _display();
     
+    _display();
+
+    //Save_a_loaded_reading();
+    Save_an_unloaded_reading();
   }//end timer
 }//end loop()
 
+void Save_a_loaded_reading(){
+  myFile = SD.open(filename, FILE_WRITE);
+  // if the file opened okay, write to it:
+  if (myFile) {
+    //cell labels
+    //loaded reading unix time, current, cell 1 mv, cell 2 mv, cell 3 mv, cell 4 mv, unloaded reading unix time, current, cell 1 mv, cell 2 mv, cell 3 mv, cell 4 mv");
+    myFile.print(now.unixtime() + ",");
+    myFile.print(bms.get_current());
+    myFile.print(",");
+    //loop prints each cell voltage. This version is only for 4 cell BMSs
+    myFile.print((bms.get_cell_voltage(0) *1000), 0);
+    myFile.print(",");
+    myFile.print((bms.get_cell_voltage(1) *1000), 0);
+    myFile.print(",");
+    myFile.print((bms.get_cell_voltage(2) *1000), 0);
+    myFile.print(",");
+    myFile.print((bms.get_cell_voltage(3) *1000), 0);
+    myFile.print(",");
+    // close the file:
+   myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    lcd.clear();
+    lcd.print("error loaded");
+    delay(4000);
+   }
+}
+
+void Save_an_unloaded_reading(){
+  myFile = SD.open(filename, FILE_WRITE);
+  // if the file opened okay, write to it:
+  if (myFile) {
+    //cell labels
+    //loaded reading unix time, current, cell 1 mv, cell 2 mv, cell 3 mv, cell 4 mv, unloaded reading unix time, current, cell 1 mv, cell 2 mv, cell 3 mv, cell 4 mv");
+    myFile.print(now.unixtime(), ",");
+    myFile.print(",");
+    myFile.print(bms.get_current());
+    myFile.print(",");
+    myFile.print((bms.get_cell_voltage(0) *1000), 0);
+    myFile.print(",");
+    myFile.print((bms.get_cell_voltage(1) *1000), 0);
+    myFile.print(",");
+    myFile.print((bms.get_cell_voltage(2) *1000), 0);
+    myFile.print(",");
+    myFile.print((bms.get_cell_voltage(3) *1000), 0);
+    myFile.println();
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    lcd.clear();
+    lcd.print(F("error unloaded"));
+    delay(4000);
+   }
+}
 
 void NewFile(){
     EEPROM.get(eeAddress, serialnumber);
     serialnumber++;
     filename = "test";
     filename += serialnumber;
-    filename += ".csv";
-    myFile = SD.open(filename, FILE_WRITE);
-    myFile.close();
+    filename += ".txt";
+    File myFile = SD.open(filename, FILE_WRITE);
+    // if the file opened okay, write to it:
+    delay(1);
+    if (myFile) {
+      //print the spreadsheet cell labels
+      myFile.println(F("loaded reading unix time,current,cell 1 mv,cell 2 mv,cell 3 mv,cell 4 mv,unloaded reading unix time,current,cell 1 mv,cell 2 mv,cell 3 mv,cell 4 mv"));
+      //cell_labels.print(myFile);
+      //myFile.println(" ");
+      // close the file:
+      myFile.close();
+    } else {
+      // if the file didn't open, print an error:
+      lcd.setCursor(0, 2);
+      lcd.print(F("error newfile"));
+      delay(4000);
+    }
     EEPROM.put(eeAddress, serialnumber);
 }
 
@@ -181,9 +255,9 @@ void _display(){
       }
       lcd.setCursor(0, 1);
       if (! rtc.isrunning()) {
-        lcd.println("RTC NOT running!");
+        lcd.println(F("RTC NOT running!"));
       }else{
-        lcd.print("unix time:");
+        lcd.print(F("unix time:"));
         lcd.print(now.unixtime());
       }
     }
