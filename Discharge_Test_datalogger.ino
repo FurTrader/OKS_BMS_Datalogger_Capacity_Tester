422601ea95a06066f5f1e6ab2d2d23f418f25752
FurTrader <32523605+FurTrader@users.noreply.github.com>
2022-08-29 13:21:39 -0400

    Update Discharge_Test_datalogger.ino

--- Discharge_Test_datalogger.ino
+++ Discharge_Test_datalogger.ino
@@ -1,3 +1,29 @@
+/*Datalogger and capacity tester for Overkill Solar BMSs
+ *
+ *This arduino datalogger will communicate with a BMS and record the test data on the SD card
+ *
+ *We used an arduino UNO clone and an Adafruit SD card shield.
+ *
+ *In some combinations, the Arduino's RX activity light will draw the signal too low from the BMS UART.
+ *For this setup, we had to remove the RX LED from the Arduino board.
+ *Other boards have been known to communicate without modification. The UNO rev3 has buffered status leds and should work fine, for example.
+ *The arduino Mega works well when you use Serial 1,2,or 3
+ *3.3v Arduino boards like the promicro always seem to communicate properly with the BMS. An ESP32 is also a good choice.
+ *
+ *Test procedure:
+ *
+ * The arduino will enable the BMS output for ten seconds then record the current and cell voltages in the data file.
+ * It will then disable the BMS MOSFET array for 1 second, wait for a stable reading of zero amps, and record another data set in the data file.
+ * 
+ * The test can be run with a load or charger connected to the battery.
+ * 
+ * The test is complete when the BMS goes into high or low voltage cutoff.
+ * 
+ * The SD card can then be used to transfer the data to a compuet. We used Google sheets to process the data and generate charts & graphs.
+ * 
+ * The data collected can be used to graph the charge/discharge curves both loaded and unloaded, and calculate capacity and internal resistance of each cell.
+ * 
+ */
 
 #include <bms2.h>
 #include <LiquidCrystal_I2C.h>
@@ -31,10 +57,6 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 void setup() {
   //only execute this once to initialize the eeprom location. This will set the serial number to the initial value.
   //EEPROM.put(eeAddress, serialnumber); //comment out this line after running once
-
-  //the contactor
-  pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
    
   //initialize the serial ports. 
   Serial.begin(9600);
@@ -149,7 +171,6 @@ void loop() {
   if ((millis() - lastmillis2) > 10000){ 
     
     Save_a_loaded_reading();
-    //digitalWrite(2, LOW); //contactor off
     //control FETS (charge, discharge)
     bms.set_0xE1_mosfet_control(false, false); //FETS off
 
