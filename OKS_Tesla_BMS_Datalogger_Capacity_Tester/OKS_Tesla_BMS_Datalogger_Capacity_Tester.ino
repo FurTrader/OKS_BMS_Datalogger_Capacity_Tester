0bb61ef47e1f40d5a3253b66d1aa59dae8670701
FurTrader <32523605+FurTrader@users.noreply.github.com>
2023-01-11 17:55:38 -0500

    Update OKS_Tesla_BMS_Datalogger_Capacity_Tester.ino

--- OKS_Tesla_BMS_Datalogger_Capacity_Tester/OKS_Tesla_BMS_Datalogger_Capacity_Tester.ino
+++ OKS_Tesla_BMS_Datalogger_Capacity_Tester/OKS_Tesla_BMS_Datalogger_Capacity_Tester.ino
@@ -44,6 +44,8 @@ DateTime now;
 //global variables
 long lastmillis;
 long lastmillis2;
+long lastmillis3;
+int cyclecount;
 int eeAddress = 0;   //Location we want the SN data to be put.
 int serialnumber = 1000; //starting serial number
 String filename = "test";
@@ -134,7 +136,7 @@ void setup() {
   }//end timer
   
   //control FETS (charge, discharge)
-    Fets_off();
+  Fets_off();
 
       lastmillis = millis();
   while ((millis() - lastmillis) < 2500){ // timer
@@ -156,6 +158,8 @@ void setup() {
   //print BMS model number
   delay(3000); //delay is ok here
 
+  //control FETS (charge, discharge)
+  Fets_on();
 }//end setup
 
 
@@ -165,21 +169,33 @@ void loop() {
   bms.main_task(true); //call the BMS library every loop.
   
   //1 second timer, non-blocking
+  //update the lcd every second
   if ((millis() - lastmillis) > 1000){ 
     lastmillis = millis();
-    
     _display();
-   
   }//end 1 second timer
 
-    //10 second timer, non-blocking
-  if ((millis() - lastmillis2) > 10000){ 
-    
-    Save_a_loaded_reading();
-    
-    lastmillis2 = millis();
 
+  //10 second timer, non-blocking
+  //save a loaded reading every 10 seconds
+  if ((millis() - lastmillis2) > 10000){ 
+    cyclecount++;
+    Save_a_reading();
+    lastmillis2 = millis();
   }//end 10s timer
+
+
+  //save an unloaded reading every 10 minutes (60 cycles)
+  //turn off the fets after cycle 58
+  if (cyclecount == 58){ 
+    Fets_off();
+  }//end (cyclecount == 58)
+
+  //turn on the fets after cycle 59
+  if (cyclecount == 59){ 
+    Fets_on();
+    cyclecount = 0; //reset cycle count
+  }//end (cyclecount == 59)
   
 }//end loop()
 
@@ -195,7 +211,7 @@ void Fets_on(){
   bms.set_0xE1_mosfet_control(true, true); //FETs on
 }
 
-void Save_a_loaded_reading(){
+void Save_a_reading(){
   myFile = SD.open(filename, FILE_WRITE);
   // if the file opened okay, write to it:
   if (myFile) {
@@ -222,9 +238,9 @@ void Save_a_loaded_reading(){
     myFile.print(",");
     myFile.print((bms.get_cell_voltage(5) *1000), 0);
     myFile.print(",");
-    //myFile.print(NTC 1);
+    myFile.print(bms.get_ntc_temperature(0), 0);
     myFile.print(",");
-    //myFile.print(NTC 2);
+    myFile.print(bms.get_ntc_temperature(1), 0);
     myFile.println();
     // close the file:
    myFile.close();
@@ -248,7 +264,7 @@ void NewFile(){
     delay(1);
     if (myFile) {
       //print the spreadsheet cell labels
-      myFile.println(F("loaded reading unix time,milliseconds,current,cell 1,cell 2,cell 3,cell 4,cell 5,cell 6,NTC 1,NTC 2"));
+      myFile.println(F("unix time,milliseconds,current,cell 1,cell 2,cell 3,cell 4,cell 5,cell 6,NTC 1,NTC 2"));
       //cell_labels.print(myFile);
       //myFile.println(" ");
       // close the file:
@@ -290,9 +306,9 @@ void _display(){
       if(watts > 0){ lcd.print("+"); }
       lcd.print(watts, 0);
       lcd.print("w ");
-      //lcd.setCursor(11, 0);
+      lcd.setCursor(14, 0);
       lcd.print(bms.get_voltage());
-      lcd.print(" V");
+      lcd.print("V");
       lcd.setCursor(0, 1);
       if(bms.get_current() > 0){ lcd.print("+"); } 
       lcd.print(bms.get_current());
@@ -311,7 +327,9 @@ void _display(){
         lcd.print(" ");
         lcd.print((bms.get_cell_voltage(2) *1000), 0);
         lcd.print(" ");
-        lcd.print(bms.get_ntc_temperature(0));
+        lcd.setCursor(17, 2);
+        lcd.print(bms.get_ntc_temperature(0), 0);
+        lcd.print("c");
         lcd.setCursor(0, 3);
         lcd.print((bms.get_cell_voltage(3) *1000), 0);
         lcd.print(" ");
@@ -319,7 +337,9 @@ void _display(){
         lcd.print(" ");
         lcd.print((bms.get_cell_voltage(5) *1000), 0);
         lcd.print(" ");
-        lcd.print(bms.get_ntc_temperature(1));
+        lcd.setCursor(17, 3);
+        lcd.print(bms.get_ntc_temperature(1), 0);
+        lcd.print("c");
             
     }
 
